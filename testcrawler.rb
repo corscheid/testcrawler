fe3a3ee6c1c281c4455a4f2df90f4ef57d398a64
Corey Scheideman <corey9878@gmail.com>
2017-10-04 22:59:21 -0400

    some housekeeping

--- testcrawler.rb
+++ testcrawler.rb
@@ -3,7 +3,7 @@ require 'anemone'
 require 'gmail'
 
 # print help page and exit
-if ARGV.include?("-h") || ARGV.include?("--help")
+if ARGV.include?('-h') || ARGV.include?('--help')
   puts %(testcrawler [https://github.com/corscheid/testcrawler]
   usage: $ ruby testcrawler.rb [URL] [option]
 
@@ -18,8 +18,8 @@ end
 
 # some settings
 time = Time.new
-config_file = YAML.load_file(File.dirname(__FILE__)+'/config.yaml')
-url = (ARGV.length > 0 && ARGV[0].start_with?('http')) ? ARGV[0] : config_file['url']
+config_file = YAML.load_file(File.dirname(__FILE__) + '/config.yaml')
+url = !ARGV.empty? && ARGV[0].start_with?('http') ? ARGV[0] : config_file['url']
 to_addr = config_file['to_addr']
 cc_addr = config_file['cc_addr']
 to_name = config_file['to_name']
@@ -33,10 +33,10 @@ message = %(#{greeting} #{to_name},
 #{intro}
 )
 results_body = ''
-quiet = ARGV.include?("-q") || ARGV.include?("--quiet")
+quiet = ARGV.include?('-q') || ARGV.include?('--quiet')
 
 # setup the output file
-log_file = File.open(File.dirname(__FILE__)+'/testcrawler.log', 'a')
+log_file = File.open(File.dirname(__FILE__) + '/testcrawler.log', 'a')
 log_file.puts "#{time.inspect}\n\n"
 
 # crawl the site and gather urls, codes, etc.
